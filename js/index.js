4369e365b4f3a8253469f08afb41faa7d8b681e7
petrokostereva123 <petrokostereva123@gmail.com>
2020-02-21 15:59:43 +0000

    not all js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,12 @@ const clock = document.querySelector('.clock-box')
 const showClock = () => clock.innerText = (new Date()).toLocaleTimeString();
 setInterval(showClock, 1000)
 
+function multiply() {
+    let num1, result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+    result = num1 * num1;
+    document.getElementById('numberisEqualTo').innerHTML = result;
+}
+
+
